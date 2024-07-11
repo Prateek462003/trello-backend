71d58ebdeec4a6a5c01e7abad2bda0a9511c3baf
Prateek462003 <prateeksaxena462003@gmail.com>
2024-07-12 00:24:32 +0530

    Updated import paths

--- main.go
+++ main.go
@@ -14,7 +14,6 @@ func main() {
 	database.Init()
 
 	router := gin.Default()
-	// Setup the routes
 	router.GET("/tasks", controllers.GetTasks)
 	router.POST("/tasks", controllers.CreateTask)
 	router.DELETE("/tasks/:id", controllers.DeleteTask)
