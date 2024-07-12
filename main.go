98d3de987d0ee8baf71dfe243492fc02e78321ff
Prateek462003 <prateeksaxena462003@gmail.com>
2024-07-13 00:25:58 +0530

    added activities controller

--- main.go
+++ main.go
@@ -18,13 +18,13 @@ type Task struct {
 	ID          int    `json:"id"`
 	Title       string `json:"title"`
 	Description string `json:"description"`
+	Image       string `json:"image"`
+	ActivityId  int    `json:"activity_id"`
 }
 
 type Activity struct {
-	ID                  int    `json:"id"`
-	TaskID              int    `json:"task_id"`
-	ActivityName        string `json:"activity_name"`
-	ActivityDescription string `json:"activity_description"`
+	ID   int    `json:"id"`
+	Name string `json:"name"`
 }
 
 func getTasks(c *gin.Context) {
@@ -54,7 +54,7 @@ func createTask(c *gin.Context) {
 		return
 	}
 
-	_, err := db.Exec("INSERT INTO tasks (title, description) VALUES ($1, $2)", task.Title, task.Description)
+	_, err := db.Exec("INSERT INTO tasks (title, description, image, activity_id) VALUES ($1, $2, $3, $4)", task.Title, task.Description, task.Image, task.ActivityId)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
@@ -72,6 +72,70 @@ func deleteTask(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{"message": "Task deleted"})
+}
+
+func getActivities(c *gin.Context) {
+	c.Header("Content-Type", "application/json")
+	rows, err := db.Query("SELECT id, name FROM activities")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	var activities []Activity
+	for rows.Next() {
+		var activity Activity
+		if err := rows.Scan(&activity.ID, &activity.Name); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		activities = append(activities, activity)
+	}
+	c.JSON(http.StatusOK, activities)
+}
+
+func createActivity(c *gin.Context) {
+	var activity Activity
+	if err := c.BindJSON(&activity); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	_, err := db.Exec("INSERT INTO activities (name) VALUES ($1)", activity.Name)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, activity)
+}
+
+func GetTasksByActivity(c *gin.Context) {
+	activityID := c.Param("activity_id")
+
+	rows, err := db.Query("SELECT id, title, description, image, activity_id FROM tasks WHERE activity_id = $1", activityID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	defer rows.Close()
+
+	var tasks []Task
+	for rows.Next() {
+		var task Task
+		if err := rows.Scan(&task.ID, &task.Title, &task.Description, &task.Image, &task.ActivityId); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		tasks = append(tasks, task)
+	}
+
+	if err = rows.Err(); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, tasks)
 }
 
 func main() {
@@ -92,6 +156,10 @@ func main() {
 
 	router := gin.Default()
 
+	router.GET("/activities", getActivities)
+	router.POST("/activities", createActivity)
+	router.GET("/activities/:activity_id", GetTasksByActivity)
+
 	router.GET("/tasks", getTasks)
 	router.POST("/tasks", createTask)
 	router.DELETE("/tasks/:id", deleteTask)
@@ -100,5 +168,5 @@ func main() {
 	if port == " " {
 		port = "8080"
 	}
-	router.Run(":" + port)
+	router.Run("localhost:" + port)
 }
